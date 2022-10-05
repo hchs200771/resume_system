dd0a73fff5e2fbdf82e30f11c18c0cf0371690e3
max <hchs200771@gmail.com>
2022-10-05 11:33:10 +0800

    first commit

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_014616) do
+  create_table "candidates", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.string "name"
+    t.integer "candidate_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["candidate_id"], name: "index_resumes_on_candidate_id"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string "title"
+    t.integer "resume_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["resume_id"], name: "index_templates_on_resume_id"
+  end
+
+  create_table "work_experiences", force: :cascade do |t|
+    t.integer "candidate_id", null: false
+    t.string "company"
+    t.integer "years"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["candidate_id"], name: "index_work_experiences_on_candidate_id"
+  end
+
+  add_foreign_key "resumes", "candidates"
+  add_foreign_key "templates", "resumes"
+  add_foreign_key "work_experiences", "candidates"
+end
